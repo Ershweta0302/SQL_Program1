083cd2915979782f89ede11bbb0031fdddcd4424
Ershweta0302 <95718381+Ershweta0302@users.noreply.github.com>
2022-07-09 17:50:20 +0530

    Store Procedure
    Insert , Update , Delete , GetAllUser , Login

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,17 +1,113 @@
-create database Employeeinfo
-use Employeeinfo
-create table EmployeeDetails(id int, Name varchar(25), Department varchar(15))
-select * from EmployeeDetails
-insert into EmployeeDetails(id,Name,Department) values(1001,'Rahul Kumar' , 'Web Developer')
-insert into EmployeeDetails(id,Name,Department) values(1002,'Raj Kumar' , 'Web Designer')
-insert into EmployeeDetails(id,Name,Department) values(1001,'Saloni Kumari' , '.net Developer')
-insert into EmployeeDetails(id,Name,Department) values(1001,'Rajan Kumar' , 'Upi Developer')
-insert into EmployeeDetails(id,Name,Department) values(1001,'Amit Sing' , 'Php Developer')
-insert into EmployeeDetails(id,Name,Department) values(1001,'Joti Singh' , 'Java Developer')
+create database StorDB
+use StorDB
 
-create table EmployeeInformation(id int identity , Name varchar(25) ,Father_Name varchar (25), Addres varchar(50),
-Qulifation varchar (10) , Nationalti varchar(15))
-select * from EmployeeInformation
-insert into EmployeeInformation (id,Name,Father_Name,Addres,Qulifation,Nationalti)
-values(1001, 'Rohit Kumar','Rakesh Kumar','Bhopal','B.C.A','Indian')
-values(1002, 'Ansika Kumari','Rakesh Singh','Delhi','B.tech','Indian')
+CREATE TABLE[EmpRegistation](      
+    [Id] [int] IDENTITY(1,1) NOT NULL,      
+    [FirstName] [varchar](500) NOT NULL,    
+    [LastName] [varchar](500) NOT NULL, 
+	[PhoneNo][varchar](20) NOT NULL,
+    [Email] [varchar](500) NOT NULL,   
+    [Password] [varchar](500) NOT NULL,  
+    [City] [varchar](50) NOT NULL,      
+   )  
+
+   CREATE TABLE EmpLogin
+   (
+   Id int primary key identity,
+   Email varchar (500),
+   Password varchar(200)
+   )
+   ---STORE PEROCDURE ---
+
+CREATE procedure [dbo].[Usp_InsertUpdateDelete_Emp]  
+@ID INT = NULL  
+,@FirstName NVARCHAR(500) = NULL  
+,@LastName NVARCHAR(500) = NULL  
+,@PhoneNo NVARCHAR(15) = NULL  
+,@Email NVARCHAR(300) = NULL
+,@Password NVARCHAR(300) = NULL  
+,@City NVARCHAR(500) = NULL  
+,@Query INT  
+AS  
+BEGIN  
+IF (@Query = 1)  
+BEGIN  
+
+INSERT INTO EmpRegistation(  
+FirstName,
+LastName,
+PhoneNo,
+Email,
+Password,
+City
+) 
+VALUES (  
+@FirstName,
+@LastName,
+@PhoneNo,
+@Email,
+@Password,
+@City
+)  
+IF (@@ROWCOUNT > 0)  
+BEGIN  
+SELECT 'Insert'  
+END  
+END  
+IF (@Query = 2)  
+---- Update ----
+BEGIN  
+UPDATE EmpRegistation  
+SET FirstName = @FirstName,
+	LastName = @LastName,
+	PhoneNo = @PhoneNo,
+	City=@City
+WHERE EmpRegistation.Id =@ID  
+SELECT 'Update'  
+END  
+IF (@Query = 3)  
+
+-----Delete----
+BEGIN  
+DELETE  
+FROM EmpRegistation  
+WHERE EmpRegistation.Id=@ID --.CustomerID = @CustomerID  
+SELECT 'Deleted'  
+END  
+IF (@Query = 4)  
+BEGIN  
+SELECT *  
+FROM EmpRegistation  
+END  
+END  
+IF (@Query = 5)  
+BEGIN  
+SELECT *  
+FROM EmpRegistation
+WHERE EmpRegistation.Id=@ID 
+END
+
+----Login---
+CREATE procedure login_pro
+
+(
+
+        @Email varchar(300),
+
+        @Password varchar(300)
+
+)
+
+as
+
+declare @status int
+
+if exists(select * from EmpLogin where Email=@Email and Password=@Password)
+
+       set @status=1
+
+else
+
+       set @status=0
+
+select @status
